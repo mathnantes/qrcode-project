35723ceee07a6133ee9f1ea5b8a87d8772773338
mathnantes <mathnantes98@gmail.com>
2024-05-03 09:58:54 -0400

    fixed transitions, but broke lectures button and home dropdown menu

--- static/script.js
+++ static/script.js
@@ -2,138 +2,34 @@ document.addEventListener('DOMContentLoaded', function () {
 	const buttons = document.querySelectorAll('.nav-button');
 	const contentArea = document.getElementById('content-area');
 	const defaultContentHTML = contentArea.innerHTML; // Keep the default content
-	let isFirstLoad = true; // Flag to check if it's the first load
 
-	// Function to update content based on button id
-	function updateContentArea(buttonId) {
+	function updateContentArea(buttonId, callback) {
 		switch (buttonId) {
 			case 'home-btn':
 				contentArea.innerHTML = defaultContentHTML;
 				populateLectureDropdown();
+				callback();
 				break;
 			case 'history-btn':
-				fetchHistory();
+				fetchHistory(callback);
 				break;
 			case 'lectures-btn':
-				fetchLectures();
+				fetchLectures(callback);
 				break;
 			case 'settings-btn':
 				contentArea.innerHTML = '<div>Settings Content will go here</div>';
+				callback();
 				break;
 		}
 	}
 
-	function fetchHistory() {
-		fetch('/history')
-			.then((response) => response.json())
-			.then((data) => {
-				let historyHTML = '<div class="history-page">';
-				if (data.length) {
-					historyHTML += data
-						.map(
-							(record) => `
-                            <div class="history-card">
-                                <div class="history-info">Name: ${
-																	record.first_name
-																} ${record.last_name}</div>
-                                <div class="history-info">Organization: ${
-																	record.organization
-																}</div>
-                                <div class="history-info">Check-in: ${
-																	record.check_in_time || 'Not Checked In'
-																}</div>
-                                <div class="history-info">Check-out: ${
-																	record.check_out_time || 'Not Checked Out'
-																}</div>
-                                <div class="history-info">Lecture: ${
-																	record.lecture_name
-																}</div>
-                            </div>
-                        `
-						)
-						.join('');
-				} else {
-					historyHTML += `
-                    <div class="no-history">
-                        <div class="no-history-text">No attendance records available.</div>
-                    </div>
-                `;
-				}
-				historyHTML += '</div>'; // Close history-page div
-				contentArea.innerHTML = historyHTML;
-			})
-			.catch((error) => console.error('Error loading history:', error));
-	}
-
-	// Fetch lectures from server and update content area
-	function fetchLectures() {
-		fetch('/lectures')
-			.then((response) => response.json())
-			.then((data) => {
-				let lecturesHTML = '<div class="lectures-page">';
-				if (data.length) {
-					lecturesHTML += data
-						.map(
-							(lecture) => `
-                            <div class="lecture-card">
-                                <div class="lecture-info">Name: ${lecture.name}</div>
-                                <div class="lecture-info">Lecturer: ${lecture.lecturer}</div>
-                                <div class="lecture-info">Start Time: ${lecture.start_time}</div>
-                                <div class="lecture-info">End Time: ${lecture.end_time}</div>
-                            </div>
-                        `
-						)
-						.join('');
-				} else {
-					lecturesHTML += `
-                    <div class="no-lectures">
-                        <div class="no-lectures-text">No lectures available.</div>
-                    </div>
-                `;
-				}
-				lecturesHTML += '</div>'; // Close lectures-page div
-				lecturesHTML += `
-                <div class="add-lecture-btn" onclick="toggleLectureForm()">
-                    <img src="/static/add.png" alt="Add" style="width:50px; height:50px;">
-                </div>
-            `;
-				lecturesHTML += `
-                <form id="lecture-form" class="lecture-form" style="display:none;">
-                    <button type="button" class="close-btn" onclick="toggleLectureForm()">&#10005;</button>
-                    <h1>Register New Lecture</h1>
-                    <div class="input-group">
-                        <label for="lecture-name">Name:</label>
-                        <input type="text" id="lecture-name" class="form-input" placeholder="Enter lecture name" required>
-                    </div>
-                    <div class="input-group">
-                        <label for="lecturer-name">Lecturer:</label>
-                        <input type="text" id="lecturer-name" class="form-input" placeholder="Enter lecturer's name" required>
-                    </div>
-                    <div class="input-group">
-                        <label for="start-time">Start Time:</label>
-                        <input type="datetime-local" id="start-time" class="form-input" required>
-                    </div>
-                    <div class="input-group">
-                        <label for="end-time">End Time:</label>
-                        <input type="datetime-local" id="end-time" class="form-input" required>
-                    </div>
-                    <button type="submit" class="submit-btn">Register Lecture</button>
-                </form>
-            `;
-
-				contentArea.innerHTML = lecturesHTML;
-			})
-			.catch((error) => console.error('Error loading lectures:', error));
-	}
-
-	// Populate lectures dropdown
 	function populateLectureDropdown() {
 		fetch('/lectures')
 			.then((response) => response.json())
 			.then((lectures) => {
 				const dropdown = document.getElementById('lecture-dropdown');
 				dropdown.innerHTML =
-					'<option disabled selected>Select Lecture</option>'; // Reset dropdown
+					'<option disabled selected>Select Lecture</option>';
 				lectures.forEach((lecture) => {
 					const option = document.createElement('option');
 					option.value = lecture.id;
@@ -144,27 +40,115 @@ document.addEventListener('DOMContentLoaded', function () {
 			.catch((error) => console.error('Failed to load lectures:', error));
 	}
 
-	// Initialize page without transitions
-	updateContentArea('home-btn'); // Directly set initial content without transition
-	document.querySelector('#home-btn').classList.add('selected');
-	isFirstLoad = false; // Ensure transitions are allowed for subsequent clicks
+	function fetchHistory(callback) {
+		fetch('/history')
+			.then((response) => response.json())
+			.then((data) => {
+				let historyHTML = '<div class="history-page">';
+				if (data.length) {
+					historyHTML += data
+						.map(
+							(record) => `
+                        <div class="history-card">
+                            <div class="history-info">Name: ${
+															record.first_name
+														} ${record.last_name}</div>
+                            <div class="history-info">Organization: ${
+															record.organization
+														}</div>
+                            <div class="history-info">Check-in: ${
+															record.check_in_time || 'Not Checked In'
+														}</div>
+                            <div class="history-info">Check-out: ${
+															record.check_out_time || 'Not Checked Out'
+														}</div>
+                            <div class="history-info">Lecture: ${
+															record.lecture_name
+														}</div>
+                        </div>
+                    `
+						)
+						.join('');
+				} else {
+					historyHTML +=
+						'<div class="no-history"><div class="no-history-text">No attendance records available.</div></div>';
+				}
+				historyHTML += '</div>';
+				contentArea.innerHTML = historyHTML;
+				callback();
+			})
+			.catch((error) => console.error('Error loading history:', error));
+	}
+
+	function fetchLectures(callback) {
+		fetch('/lectures')
+			.then((response) => response.json())
+			.then((data) => {
+				let lecturesHTML = '<div class="lectures-page">';
+				if (data.length) {
+					lecturesHTML += data
+						.map(
+							(lecture) => `
+                        <div class="lecture-card">
+                            <div class="lecture-info">Name: ${lecture.name}</div>
+                            <div class="lecture-info">Lecturer: ${lecture.lecturer}</div>
+                            <div class="lecture-info">Start Time: ${lecture.start_time}</div>
+                            <div class="lecture-info">End Time: ${lecture.end_time}</div>
+                        </div>
+                    `
+						)
+						.join('');
+				} else {
+					lecturesHTML +=
+						'<div class="no-lectures"><div class="no-lectures-text">No lectures available.</div></div>';
+				}
+				lecturesHTML += '</div>';
+				lecturesHTML += `
+                    <div class="add-lecture-btn" style="position: fixed; bottom: 10%; left: 50%; transform: translateX(-50%);">
+                        <img src="/static/add.png" alt="Add Lecture" style="width:50px; height:50px;" onclick="toggleLectureForm()">
+                    </div>
+                    <form id="lecture-form" class="lecture-form" style="display:none;">
+                        <button type="button" class="close-btn" onclick="toggleLectureForm()">&#10005;</button>
+                        <h1>Register New Lecture</h1>
+                        <div class="input-group">
+                            <label for="lecture-name">Name:</label>
+                            <input type="text" id="lecture-name" class="form-input" placeholder="Enter lecture name" required>
+                        </div>
+                        <div class="input-group">
+                            <label for="lecturer-name">Lecturer:</label>
+                            <input type="text" id="lecturer-name" class="form-input" placeholder="Enter lecturer's name" required>
+                        </div>
+                        <div class="input-group">
+                            <label for="start-time">Start Time:</label>
+                            <input type="datetime-local" id="start-time" class="form-input" required>
+                        </div>
+                        <div class="input-group">
+                            <label for="end-time">End Time:</label>
+                            <input type="datetime-local" id="end-time" class="form-input" required>
+                        </div>
+                        <button type="submit" class="submit-btn">Register Lecture</button>
+                    </form>
+                `;
+				contentArea.innerHTML = lecturesHTML;
+				callback();
+			})
+			.catch((error) => console.error('Error loading lectures:', error));
+	}
 
 	buttons.forEach((button) => {
 		button.addEventListener('click', function () {
-			// Remove selected class from all buttons and add to the clicked one
 			buttons.forEach((btn) => btn.classList.remove('selected'));
 			this.classList.add('selected');
-
-			// Add transition effects
 			contentArea.classList.add('fade-out');
 			setTimeout(() => {
-				updateContentArea(this.id);
-				contentArea.classList.remove('fade-out');
-				contentArea.classList.add('fade-in');
-				setTimeout(() => {
-					contentArea.classList.remove('fade-in');
-				}, 500); // Ensure fade-in class is removed after animation
-			}, 500); // Wait for fade out to complete before updating content
+				updateContentArea(this.id, () => {
+					contentArea.classList.remove('fade-out');
+					contentArea.classList.add('fade-in');
+					setTimeout(() => {
+						contentArea.classList.remove('fade-in');
+					}, 500);
+				});
+			}, 500);
 		});
 	});
 
@@ -188,9 +172,9 @@ document.addEventListener('DOMContentLoaded', function () {
 				})
 					.then((response) => response.json())
 					.then((data) => {
-						console.log(data.message); // You can show this message in the UI
-						fetchLectures(); // Refresh the list of lectures
-						toggleLectureForm(); // Hide the form after registration
+						console.log(data.message);
+						fetchLectures(() => {});
+						toggleLectureForm();
 					})
 					.catch((error) => console.error('Error registering lecture:', error));
 			}
@@ -199,33 +183,30 @@ document.addEventListener('DOMContentLoaded', function () {
 	document
 		.getElementById('triggerCamera')
 		.addEventListener('click', function (event) {
-			event.preventDefault(); // Prevent default behavior
-			document.getElementById('cameraInput').click(); // Open file dialog
+			event.preventDefault();
+			document.getElementById('cameraInput').click();
 		});
 
 	let selectedFile = null;
-
-	// Event listener for file input change to store the file temporarily
 	document
 		.getElementById('cameraInput')
 		.addEventListener('change', function (event) {
 			if (event.target.files.length > 0) {
-				selectedFile = event.target.files[0]; // Store file temporarily
+				selectedFile = event.target.files[0];
 			}
 		});
 
 	function clearForm() {
-		document.getElementById('cameraInput').value = ''; // Reset file input
-		document.querySelector('input[name="attendance"]:checked').checked = false; // Uncheck radio buttons
-		document.getElementById('lecture-dropdown').selectedIndex = 0; // Reset dropdown to default option
-		selectedFile = null; // Clear the stored file
+		document.getElementById('cameraInput').value = '';
+		document.querySelector('input[name="attendance"]:checked').checked = false;
+		document.getElementById('lecture-dropdown').selectedIndex = 0;
+		selectedFile = null;
 	}
 
 	document
 		.querySelector('.submit-btn')
 		.addEventListener('click', function (event) {
-			event.preventDefault(); // Prevent form submission
-
+			event.preventDefault();
 			if (!selectedFile) {
 				alert('Please select an image to scan.');
 				return;
@@ -238,7 +219,6 @@ document.addEventListener('DOMContentLoaded', function () {
 				alert('Please select a lecture.');
 				return;
 			}
-
 			const formData = new FormData();
 			formData.append('file', selectedFile);
 			formData.append(
@@ -249,7 +229,6 @@ document.addEventListener('DOMContentLoaded', function () {
 				'lecture_id',
 				document.getElementById('lecture-dropdown').value
 			);
-
 			fetch('/scan', {
 				method: 'POST',
 				body: formData,
@@ -261,33 +240,33 @@ document.addEventListener('DOMContentLoaded', function () {
 					} else {
 						console.log('QR Code Data:', data.decoded_data);
 						alert('Attendance recorded successfully!');
-						clearForm(); // Clear form on successful submission
+						clearForm();
 					}
 				})
 				.catch((error) => {
 					console.error('Error:', error);
-					clearForm(); // Clear form even if there is an error
+					clearForm();
 				});
 		});
-});
 
-// Function to toggle lecture form visibility
-function toggleLectureForm() {
-	const form = document.querySelector('.lecture-form');
-	const addButton = document.querySelector('.add-lecture-btn img');
-	if (form.style.display === 'none' || !form.style.display) {
-		form.style.display = 'block';
-		addButton.style.opacity = '0.5';
-		setTimeout(() => {
-			form.style.opacity = '1';
-			form.style.transform = 'translate(-50%, -50%) scale(1)';
-		}, 10); // Small delay to allow CSS to apply display block before transforming
-	} else {
-		form.style.transform = 'translate(-50%, -50%) scale(0.1)';
-		form.style.opacity = '0';
-		setTimeout(() => {
-			form.style.display = 'none';
-			addButton.style.opacity = '1';
-		}, 500); // Match timeout to transition time
-	}
-}
+	// Function to toggle lecture form visibility
+	window.toggleLectureForm = function () {
+		const form = document.querySelector('.lecture-form');
+		const addButton = document.querySelector('.add-lecture-btn img');
+		if (form.style.display === 'none' || !form.style.display) {
+			form.style.display = 'block';
+			addButton.style.opacity = '0.5';
+			setTimeout(() => {
+				form.style.opacity = '1';
+				form.style.transform = 'translate(-50%, -50%) scale(1)';
+			}, 10);
+		} else {
+			form.style.transform = 'translate(-50%, -50%) scale(0.1)';
+			form.style.opacity = '0';
+			setTimeout(() => {
+				form.style.display = 'none';
+				addButton.style.opacity = '1';
+			}, 500);
+		}
+	};
+});
